60ca549c8adf4b691318ec4d5839e41bee43b768
Jonathan Meshesha <mesheshajon2002@gmail.com>
2022-01-22 22:53:21 -0600

    feat: moved help command into separate function

--- src/main/java/listener/BotListener.java
+++ src/main/java/listener/BotListener.java
@@ -37,27 +37,31 @@ public class BotListener implements EventListener {
 
         switch (messageText.toLowerCase()){
             case "!help":
+                helpCommand(message);
 
-                message.reply("Commands:\n" +
-                        prefix + "help\n" +
-                            "\t Usage:" + prefix + "help\n" +
-                            "\t Description: Gives a list of commands \n" +
-                        prefix + "track\n" +
-                            "\t Usage: " + prefix + "track [email or accountname] {the email or account name you want to track}\n" +
-                            "\t Description: notifies you if the account name or email is in a recent breach after each breach check\n" +
-                            "\t Example: " + prefix + "track email testemail@gmail.com \n"+
-                        prefix + "setinterval\n" +
-                            "\t Usage: " + prefix + "setinterval {number of days} \n" +
-                            "\t Description: sets the amount of days it waits to checks for breaches \n" +
-                            "\t Example: " + prefix + "setinterval 5 \n"+
-                        prefix + "setchannel\n" +
-                            "\t Usage: " + prefix + "setchannel {the text channel which is sent breach info}\n" +
-                            "\t Description: sets the channel that the bot will send breach information to \n" +
-                            "\t Example: " + prefix + "setchannel #general \n").queue();
         }
 
 
 
+    }
+
+    public void helpCommand(Message message){
+        message.reply("Commands:\n" +
+                prefix + "help\n" +
+                "\t Usage:" + prefix + "help\n" +
+                "\t Description: Gives a list of commands \n" +
+                prefix + "track\n" +
+                "\t Usage: " + prefix + "track [email or accountname] {the email or account name you want to track}\n" +
+                "\t Description: notifies you if the account name or email is in a recent breach after each breach check\n" +
+                "\t Example: " + prefix + "track email testemail@gmail.com \n"+
+                prefix + "setinterval\n" +
+                "\t Usage: " + prefix + "setinterval {number of days} \n" +
+                "\t Description: sets the amount of days it waits to checks for breaches \n" +
+                "\t Example: " + prefix + "setinterval 5 \n"+
+                prefix + "setchannel\n" +
+                "\t Usage: " + prefix + "setchannel {the text channel which is sent breach info}\n" +
+                "\t Description: sets the channel that the bot will send breach information to \n" +
+                "\t Example: " + prefix + "setchannel #general \n").queue();
     }
 
 }
